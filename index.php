2f65bd951e4939c5e20353a2be03b5aea60c4cab
Scott Farr <creativeeyesmedia@gmail.com>
2011-12-21 17:25:01 -0600

    merged the pages

--- index.php
+++ index.php
@@ -13,15 +13,12 @@ and open the template in the editor.
         echo "hello world!<br/>We're figuring out Git and github!";
         echo "<p>I made another change</p>";
         echo "<p>I made another change from another computer</p>";
+
         ?>
-
-
-        echo "This is the first edit in a new branch.";
-
-        
-        
         
         <p>Not really sure why I'm writing echo statements at this point.  Seems pointless.</p>
+
+
         
     </body>
 </html>
